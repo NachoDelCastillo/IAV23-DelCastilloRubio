367f7f181bf53779e944e98fcdfd92f2c2b47a66
NachoDelCastillo <nachodelcastillorubio@gmail.com>
2023-05-22 15:36:07 +0200

    Cambios en el personaje

---
+++ IA_ProyectoFinal/Assets/Scripts/FollowObject.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace NX
+{
+    public class FollowObject : MonoBehaviour
+    {
+        [SerializeField]
+        Transform target;
+
+        [SerializeField]
+        float followSpeed;
+
+        Transform playerTr;
+        Rigidbody playerRb;
+
+        float velocityLerper = 0;
+        Vector3 direction;
+        Vector3 directionLerped;
+
+        private void Awake()
+        {
+            PlayerManager player = FindObjectOfType<PlayerManager>();
+            playerTr = player.transform;
+            playerRb = player.GetComponent<Rigidbody>();
+        }
+
+        private void Update()
+        {
+            transform.position = Vector3.Lerp(transform.position, target.position, followSpeed * Time.deltaTime);
+
+            Vector3 velThisFrame = -playerRb.velocity;
+
+            if (velThisFrame != Vector3.zero)
+            {
+                direction = -playerRb.velocity;
+                direction.y = 0;
+                direction.Normalize();
+            }
+
+            float velocityMagnitude = playerRb.velocity.magnitude;
+            float velocityValueOverOne = velocityMagnitude / 20;
+
+            velocityLerper = Mathf.Lerp(velocityLerper, velocityValueOverOne, Time.deltaTime * 2);
+
+            directionLerped = Vector3.Lerp(directionLerped, direction, Time.deltaTime * 2);
+
+            Debug.Log("velocityValueOverOne = " + velocityValueOverOne);
+            Debug.Log("velocityLerper = " + velocityLerper);
+
+            transform.forward = Vector3.Lerp(transform.forward, directionLerped, velocityLerper);
+
+            transform.up = Vector3.Lerp(Vector3.up, direction, velocityLerper);
+        }
+    }
+}
